44ca3d603759e36368bbf53108cd6d52750dce8f
vishaldevani18 <67007652+vishaldevani18@users.noreply.github.com>
2020-12-14 15:10:27 +0530

    run

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -6,7 +6,7 @@ function ContactUs() {
  <h2>Contact Us</h2>
 <p>Do you have any queries? We’re happy to help!</p>
 
-<p>We love hearing our customers and their ideas to translate them into digital realities.
+<p>We love heaaaaaring our customers and their ideas to translate them into digital realities.
     Our team is available to solve all your doubts and offer you a wide range of IT services that help you take your business to the next level.
     </p>
 <h6>Don't forget to Claim your Free Quotation!</h6>   
